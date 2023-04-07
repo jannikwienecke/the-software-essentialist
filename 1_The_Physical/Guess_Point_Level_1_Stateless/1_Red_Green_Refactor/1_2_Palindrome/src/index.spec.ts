f3a8a4fc9d6e163c6c2333d368baf18f75764e22
Jannik Wienecke <jannik.wienecke@sirum.de>
2023-04-07 17:43:17 +0200

    REFACTOR:transform into stateless solution

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,53 +1,34 @@
-// Create a palindrome checker that should be able to detect
-// that a string is a palindrome;
-// that is, it is the same word or phrase in reverse.
-// This means that words like "mom" and "wow" palindromes.
-// It also means that words like "bill" are not palindromes.
-// It should still know that something is a palindrome,
-// even if the casing is off (that means that "Mom" is still a palindrome).
-// Lastly, it should also be able to detect palindromes
-// in phrases like "Was It A Rat I Saw"
-// and "Never Odd or Even" too.
-
-// mom or wow is palindrome
-// bill is not a palindrom
-// Mom is also a palindrome
-// should also detect palindromes in sentences
-
-// HELPERS
-// 1) use concretions (real examples -> mom is palindrome)
-// 2) use negations to check if we test what we think we test The Lightswitch Technique
-// 3) use parameterized examples (['mom', 'wow'])
-// 4) Declare the facts (what is it that we want to test)
-// 5) Refine also the test code
-
-import { isPalindrome } from "./index";
+// 1) mom or wow is palindrome
+// 2) bill is not a palindrom
+// 3) Mom is also a palindrome
+// 4) should also detect palindromes in sentences "Was It A Rat I Saw"
+import { isAPalindrome } from "./index";
 describe("palindrome checker", () => {
   it("knows that mom, wow, dad is a palindrome", () => {
     ["mom", "wow", "dad"].forEach((val) =>
-      expect(isPalindrome(val)).toBeTruthy()
+      expect(isAPalindrome(val)).toBeTruthy()
     );
   });
 
   it('knows that "hello", "bye" or "test" arent palindromes', () => {
     ["hello", "bye", "test"].forEach((val) =>
-      expect(isPalindrome(val)).toBeFalsy()
+      expect(isAPalindrome(val)).toBeFalsy()
     );
   });
 
   it("can handle camel case words like Mom or daD or WOW", () => {
     ["Mom", "WOW", "daD"].forEach((val) =>
-      expect(isPalindrome(val)).toBeTruthy()
+      expect(isAPalindrome(val)).toBeTruthy()
     );
   });
 
   it("can detect palindromes in sentences, such as 'Was It A Rat I Saw' ", () => {
     ["Was It A Rat I Saw"].forEach((val) =>
-      expect(isPalindrome(val)).toBeTruthy()
+      expect(isAPalindrome(val)).toBeTruthy()
     );
   });
 
   it("can detect that something is not a palindrome in sentences, such as 'Hello World' ", () => {
-    ["Hello World"].forEach((val) => expect(isPalindrome(val)).toBeFalsy());
+    ["Hello World"].forEach((val) => expect(isAPalindrome(val)).toBeFalsy());
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -6,11 +6,9 @@ function transformTextIntoLowercaseWord(text: string) {
   return text.split(" ").join("").toLocaleLowerCase();
 }
 
-export const isPalindrome = (text: string) => {
-  const singleWordLowerCase = transformTextIntoLowercaseWord(text);
-  const reversedVersion = reverseString(singleWordLowerCase);
-
-  if (singleWordLowerCase === reversedVersion) return true;
-
-  return false;
+export const isAPalindrome = (text: string) => {
+  return (
+    transformTextIntoLowercaseWord(text) ===
+    reverseString(transformTextIntoLowercaseWord(text))
+  );
 };
